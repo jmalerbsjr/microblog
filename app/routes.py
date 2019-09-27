aa2eb14c4ebca363d70aa3cf6239b7e9ed837182
Joe Malerba <joe.malerba@csgi.com>
2019-09-26 21:49:42 -0400

    Chapter 3

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from flask import render_template
 from app import app
 
 # A common pattern with decorators is to use them to register functions as callbacks for certain events
@@ -5,5 +6,15 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
-
+    user = {'username': 'Joseph'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home',user=user,posts=posts)
